d335bacbbf31a658a842c1f5014c0f678fcaa71b
abu jafar <you@example.com>
2022-03-17 16:07:47 +0600

    create component and pass props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 
 function App() {
+  const nayoks = ['razak', 'alomgir', 'riaz', 'maruf'];
   return (
     <div>
-      <h1 className='text-center'>Hello React</h1>
+      {
+        nayoks.map((nayok) => <Person name={nayok} />)
+      }
     </div>
   );
 }
 
+function Person({ name }) {
+  return (
+    <div className='person'>
+      <h1>{name ? name : 'name'}</h1>
+    </div>
+  )
+}
 export default App;
