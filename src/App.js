3b6d2e72dd52ad8caeff6ec200be888846e0d786
abu jafar <you@example.com>
2022-03-17 16:11:24 +0600

    clear app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,13 @@
 import './App.css';
 
 function App() {
-  const nayoks = ['razak', 'alomgir', 'riaz', 'maruf'];
+  
   return (
     <div>
-      {
-        nayoks.map((nayok) => <Person name={nayok} />)
-      }
+      
     </div>
   );
 }
 
-function Person({ name }) {
-  return (
-    <div className='person'>
-      <h1>{name ? name : 'name'}</h1>
-    </div>
-  )
-}
+
 export default App;
